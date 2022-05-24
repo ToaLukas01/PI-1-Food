6bba024ee30f342cf900ebc075d301aaed627a30
ToaLukas01 <lucasacua@gmail.com>
2022-05-24 01:21:24 -0300

    asad

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const { Op, Recipe, Dietas } = require('../db');
 
 const router = Router();
 
