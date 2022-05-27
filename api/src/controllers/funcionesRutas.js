a6dee1ba52c93c4b74bfb5a3240115c1f22a6768
ToaLukas01 <lucasacua@gmail.com>
2022-05-27 17:07:04 -0300

    modularizando funciones y validando el POST de la ruta

--- api/src/controllers/funcionesRutas.js
+++ api/src/controllers/funcionesRutas.js
@@ -22,7 +22,7 @@ async function getApiRecetas() {
     } catch(err){
         console.log(err)
     }
-}
+};
 
 async function getDbRecetas (){
     return await Recipe.findAll({ include: {
@@ -32,19 +32,71 @@ async function getDbRecetas (){
             attributes: []
         }
     }})
-}
+};
 
 async function getAllRecetas(){
     const recetasApi = await getApiRecetas();
     const recetasDb = await getDbRecetas();
     const recetas = await recetasApi.concat(recetasDb);
     return recetas
+};
+
+async function crearDietas(){
+    const tipos = ["Gluten Free", "Ketogenic", "Vegetarian", "Lacto-Vegetarian", "Ovo-Vegetarian", 
+    "Vegan", "Pescetarian", "Paleo", "Primal", "Low FODMAP", "Whole30"];
+    tipos.forEach(dieta => {
+        Dietas.findOrCreate({ where: { name: dieta }})
+    })
+    const tipoDietas = await Dietas.findAll();
+    return (tipoDietas)
+};
+
+async function crearReceta(id, name, resumen, nivelSalud, imagen, pasos, tipoDietas){
+    const receta = await Recipe.create({
+        id: id,
+        name: name,
+        resumen: resumen,
+        nivelSalud: nivelSalud,
+        imagen: imagen,
+        pasos: pasos
+    })
+    const dieta = await Dietas.findAll({ where: { name: tipoDietas }})
+    receta.addDietas(dieta);
+    return receta
 }
 
-
+function validarAtributos(id, name, resumen, nivelSalud, imagen, pasos, tipoDietas){
+    if(!id || (typeof id !== "number") || id < 0){
+        return "El id de la receta debe existir y debe ser un numero enetro positivo"
+    } else if (!name || (typeof name !== "string") || (name.length < 0) ){
+        return "El nombre de la receta debe existir y debe ser una cadena de caracteres"
+    } else if (!resumen || (typeof resumen !== "string") || (resumen.length < 0) ){
+        return "El resumen de la receta debe existir y debe ser una cadena de texto"
+    } else if (nivelSalud && typeof nivelSalud !== "number"){
+        return "EL nivel de salud de la receta debe ser un numero o estar vacio"
+    } else if (imagen && typeof imagen !== "string"){
+        return "La imagen de la receta debe ser un link o estar vacia"
+    } else if (pasos && typeof pasos !== "string" ){
+        return "Los pasos a seguir de la receta debe ser una cadena de texto o estar vacios"
+    // } else if (Array.isArray(tipoDietas)){
+    //      for(let i=0; i<tipoDietas.length; i++){
+    //          if (tipoDietas[i] !== "Gluten Free" || tipoDietas[i] !== "Ketogenic" || tipoDietas[i] !== "Vegetarian" || tipoDietas[i] !== "Lacto-Vegetarian" || tipoDietas[i] !== "Ovo-Vegetarian" || tipoDietas[i] !==  "Vegan" || tipoDietas[i] !== "Pescetarian" || tipoDietas[i] !== "Paleo" || tipoDietas[i] !== "Primal" ||tipoDietas[i] !== "Low FODMAP" ||tipoDietas[i] !== "Whole30"){
+    //              return `El tipo de dieta (${tipoDietas[i]}) no es valido`
+    //          }
+    //      };
+    //      return true
+    // } else if (tipoDietas !== "Gluten Free" || tipoDietas !== "Ketogenic" || tipoDietas !== "Vegetarian" || tipoDietas !== "Lacto-Vegetarian" || tipoDietas !== "Ovo-Vegetarian" || tipoDietas !==  "Vegan" || tipoDietas !== "Pescetarian" || tipoDietas !== "Paleo" || tipoDietas !== "Primal" ||tipoDietas !== "Low FODMAP" ||tipoDietas !== "Whole30"){
+    //     return "El tipo de dieta a ingresar debe ser uno valido"
+    } else {
+        return true
+    }
+}
 
 module.exports = {
     getApiRecetas,
     getDbRecetas,
     getAllRecetas,
+    crearDietas,
+    crearReceta,
+    validarAtributos,
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 
 const { Router } = require('express');
 const router = Router();
-const { getApiRecetas, getDbRecetas, getAllRecetas} =require("../controllers/funcionesRutas")
+const { getApiRecetas, getDbRecetas, getAllRecetas, crearDietas, crearReceta, validarAtributos } =require("../controllers/funcionesRutas")
 const { Op, Recipe, Dietas } = require('../db');
 
 
@@ -27,6 +27,7 @@ router.get("/recipes", async(req, res, next)=>{
     }   
 });
 
+
 // GET /recipes/{idReceta}:
 // Obtener el detalle de una receta en particular
 // Debe traer solo los datos pedidos en la ruta de detalle de receta
@@ -48,26 +49,21 @@ router.get("/recipes/:id", async(req, res, next)=>{
     }
 });
 
+
 // POST /recipes:
 // Recibe los datos recolectados desde el formulario controlado de la ruta de creación de recetas por body
 // Crea una receta en la base de datos relacionada con sus tipos de dietas.
 router.post("/recipes", async(req, res, next)=>{
     try {
-        const { id, name, resumen, nivelSalud, imagen, pasos, tipoDietas } = req.body
-        const receta = await Recipe.create({
-            id: id,
-            name: name,
-            resumen: resumen,
-            nivelSalud: nivelSalud,
-            imagen: imagen,
-            pasos: pasos
-        })
-        const dieta = await Dietas.findAll({ where: { name: tipoDietas }})
-        receta.addDietas(dieta);
-        if(receta){
-            return res.status(201).send("Receta creada exitosamente")
+        const { id, name, resumen, nivelSalud, imagen, pasos, tipoDietas } = req.body;
+        const validacion = validarAtributos(id, name, resumen, nivelSalud, imagen, pasos, tipoDietas);
+        if (validacion === true){
+            const receta = await crearReceta(id, name, resumen, nivelSalud, imagen, pasos, tipoDietas)
+            if(receta){
+                return res.status(201).send("Receta creada exitosamente")
+            } 
         } else {
-            return res.status(404).send("No se pudo crear la receta")
+            return res.status(404).send(validacion)
         }
     } catch (err){
         next(err)
@@ -75,24 +71,56 @@ router.post("/recipes", async(req, res, next)=>{
 });
 
 
-
-
 // GET /diets:
 // Obtener todos los tipos de dieta posibles
 // En una primera instancia, cuando no exista ninguno, 
 // deberán precargar la base de datos con los tipos de datos indicados por spoonacular
 router.get("/diets", async(req, res, next)=>{
-    const tipos = ["Gluten Free", "Ketogenic", "Vegetarian", "Lacto-Vegetarian", "Ovo-Vegetarian", 
-    "Vegan", "Pescetarian", "Paleo", "Primal", "Low FODMAP", "Whole30"]
     try {
-        tipos.forEach(dieta => {
-            Dietas.findOrCreate({ where: { name: dieta }})
-        })
-        const tipoDietas = await Dietas.findAll();
-        return res.send(tipoDietas)
+        const tipoDietas = await crearDietas();
+        return res.send(tipoDietas);
     } catch (err){
         next(err)
     }
 });
 
 module.exports = router;
+
+
+// router.post("/recipes", async(req, res, next)=>{
+//     try {
+//         const { id, name, resumen, nivelSalud, imagen, pasos, tipoDietas } = req.body
+//         const receta = await Recipe.create({
+//             id: id,
+//             name: name,
+//             resumen: resumen,
+//             nivelSalud: nivelSalud,
+//             imagen: imagen,
+//             pasos: pasos
+//         })
+//         const dieta = await Dietas.findAll({ where: { name: tipoDietas }})
+//         receta.addDietas(dieta);
+//         if(receta){
+//             return res.status(201).send("Receta creada exitosamente")
+//         } else {
+//             return res.status(404).send("No se pudo crear la receta")
+//         }
+//     } catch (err){
+//         next(err)
+//     }
+// });
+
+
+// router.get("/diets", async(req, res, next)=>{
+//     const tipos = ["Gluten Free", "Ketogenic", "Vegetarian", "Lacto-Vegetarian", "Ovo-Vegetarian", 
+//     "Vegan", "Pescetarian", "Paleo", "Primal", "Low FODMAP", "Whole30"]
+//     try {
+//         tipos.forEach(dieta => {
+//             Dietas.findOrCreate({ where: { name: dieta }})
+//         })
+//         const tipoDietas = await Dietas.findAll();
+//         return res.send(tipoDietas)
+//     } catch (err){
+//         next(err)
+//     }
+// });
